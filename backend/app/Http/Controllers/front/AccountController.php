9cf2054e7c56c70ce36770ba67a3c03084358fa9
ganeshsuryawansh <ganeshsuryawanshi594@gmail.com>
2025-07-15 12:41:13 +0530

    registration and login api

---
+++ backend/app/Http/Controllers/front/AccountController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\front;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AccountController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:5',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+
+        // Error Handling
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        // Save Data Into DB
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => "User Register Successfully!"
+        ], 200);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // Error Handling
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = User::find(Auth::user()->id);
+            $token = $user->createToken('token')->plainTextToken;
+
+            return response()->json([
+                'status' => 200,
+                'token' => $token,
+                'name' => $user->name,
+                'id' => $user->id,
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 401,
+                'errors' => 'Invalid Credentials'
+            ], 401);
+        }
+    }
+}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
 import MyCourses from "./components/pages/account/MyCourses";
-import CoursesEnrolled from "./components/pages/CoursesEnrolled";
 import WatchCourse from "./components/pages/account/WatchCourse";
 import ChangePassword from "./components/pages/account/ChangePassword";
+import MyLearning from "./components/pages/MyLearning";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
@@ -21,10 +22,16 @@ function App() {
           <Route path="/account/register" element={<Register />} />
           <Route path="/account/my-courses" element={<MyCourses />} />
           <Route path="/account/Change-Password" element={<ChangePassword />} />
-          <Route path="/account/courses-enrolled" element={<CoursesEnrolled />} />
+          <Route path="/account/courses-enrolled" element={<MyLearning />} />
           <Route path="/account/WatchCourse" element={<WatchCourse />} />
         </Routes>
       </BrowserRouter>
+
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      />
+
     </>
   )
 }
